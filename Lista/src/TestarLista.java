8daebbcfa15dec321b52fe41eb9a3a01b0b8ba0f
Felipe <felipefggw@gmail.com>
2023-05-04 22:11:55 -0300

    Deletei as classes

--- Lista/src/TestarLista.java
+++
@@ -1,8 +0,0 @@
-
-public class TestarLista {
-	public static void main(String[] args) {
-		ListaSimples lista = new ListaSimples();
-		System.out.println(lista.isempty());
-		System.out.println(lista.getInicio());
-	}
-}
